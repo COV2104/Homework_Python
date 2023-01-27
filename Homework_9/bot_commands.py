c56f6ea3c56b87162e2d594049c6c6aff07d6662
COV <tsehlo@mail.ru>
2023-01-27 16:54:17 +0300

    Домашняя работа № 9

---
+++ Homework_9/bot_commands.py
@@ -0,0 +1,28 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+
+
+async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'/hi\n/time\n/help\n/sum\n/days_until_NY\n')
+
+async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'{datetime.datetime.now().time()}')
+
+async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    items = msg.split(' ')
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x+y}')      
+
+async def daysNY_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    now = datetime.datetime.today()
+    NY = datetime.datetime(2024, 1, 1)
+    d = NY-now  
+    mm, ss = divmod(d.seconds, 60)
+    hh, mm = divmod(mm, 60)
+    await update.message.reply_text(f'До нового года: {d.days} дней {hh} ч {mm} мин {ss} сек.')          
---
+++ Homework_9/main.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+
+app = ApplicationBuilder().token("5727827246:AAEfij2U3WRjLpE6_pDXMSL9AV0CTYeAyiU").build()
+
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("days_until_NY", daysNY_command))
+
+print('server start')
+app.run_polling()
